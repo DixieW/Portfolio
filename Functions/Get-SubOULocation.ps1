8547459a1e4ceccbd31938871075187b3b6c5019
DixieW <67695995+DixieW@users.noreply.github.com>
2025-07-07 14:26:35 +0200

    Update Get-SubOULocation.ps1

--- Functions/Get-SubOULocation.ps1
+++ Functions/Get-SubOULocation.ps1
@@ -4,10 +4,10 @@
         [parameter(Mandatory=$true, HelpMessage="Enter Parent OU Name.")]
         [string]$OUName,
         [parameter()]
-        [string]$ParentOU = "OU=$OUName,OU=BeheerPartij5,OU=Productie,DC=intern,DC=ubnet,DC=nl",
+        [string]$ParentOU = "OU=$OUName,OU=,OU=,DC=,DC=,DC=",
         [parameter()]
-        [string]$server = "Wes07pwadc01"
+        [string]$server = "" # Domain Controller
     )
     Get-ADOrganizationalUnit -Server $server -SearchBase $ParentOU -Filter * -SearchScope OneLevel | Where-Object {$_.Name -ne "Shares" -and $_.Name -ne "Langdurig afwezig" -and $_.Name -ne "Groups" -and $_.Name -ne "ClientImport" -and $_.Name -ne "Uitdienst"}
 }
-(Get-SubOULocations -OUName "RU269").Name
+
